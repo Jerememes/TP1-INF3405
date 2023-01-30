1610de924a7c3646861542b55aa4f2dc44927c4a
Jerome <jerome.laporte@mines-ales.org>
2023-01-30 17:45:48 -0500

    add ls, mkdir

--- src/Client.java
+++ src/Client.java
@@ -1,21 +1,37 @@
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 // Application client
 public class Client {
-private static Socket socket;
-public static void main(String[] args) throws Exception {
-// Adresse et port du serveur
-String serverAddress = "127.0.0.1";
-int port = 5000;
-// Création d'une nouvelle connexion aves le serveur
-socket = new Socket(serverAddress, port);
-System.out.format("Serveur lancé sur [%s:%d]", serverAddress, port);
-// Céatien d'un canal entrant pour recevoir les messages envoyés, par le serveur
-DataInputStream in = new DataInputStream(socket.getInputStream());
-// Attente de la réception d'un message envoyé par le, server sur le canal
-String helloMessageFromServer = in.readUTF();
-System.out.println(helloMessageFromServer);
-// fermeture de La connexion avec le serveur
-socket.close();
-}
+	private static Socket socket;
+	public static void main(String[] args) throws Exception {
+		// Adresse et port du serveur
+		String serverAddress = "127.0.0.1";
+		int port = 5000;
+		// Création d'une nouvelle connexion aves le serveur
+		socket = new Socket(serverAddress, port);
+		System.out.format("Serveur lancé sur [%s:%d]", serverAddress, port);
+		// Céatien d'un canal entrant pour recevoir les messages envoyés, par le serveur
+		DataInputStream in = new DataInputStream(socket.getInputStream());
+		DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+		// Attente de la réception d'un message envoyé par le, server sur le canal
+		String helloMessageFromServer = in.readUTF();
+		System.out.println(helloMessageFromServer);
+		
+		Scanner scanner = new Scanner(System.in);
+		while(true) {
+			System.out.print("Enter a command: ");
+            String command = scanner.nextLine();
+            if(command.equals("exit")) {
+            	break;
+            }
+            out.writeUTF(command);
+
+            String response = in.readUTF();
+            System.out.println(response);
+		}
+		// fermeture de La connexion avec le serveur
+		socket.close();
+	}
 }
--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -1,28 +1,77 @@
 
 import java.io.DataOutputStream;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.io.File;
 public class ClientHandler extends Thread { // pour traiter la demande de chaque client sur un socket particulier
-private Socket socket;
-private int clientNumber;
-public ClientHandler(Socket socket, int clientNumber) {
-this.socket = socket;
-this.clientNumber = clientNumber;
-System.out.println("New connection with client#" + clientNumber + " at" + socket);
-}
-public void run() { // Création de thread qui envoi un message à un client
-try {
-DataOutputStream out = new DataOutputStream(socket.getOutputStream()); // création de canal d’envoi
-out.writeUTF("Hello from server - you are client#" + clientNumber); // envoi de message
-} catch (IOException e) {
-System.out.println("Error handling client# " + clientNumber + ": " + e);
-} finally {
-try {
-socket.close();
-} catch (IOException e) {
-System.out.println("Couldn't close a socket, what's going on?");
-}
-System.out.println("Connection with client# " + clientNumber + " closed");
-}
-}
+	private Socket socket;
+	private int clientNumber;
+	public ClientHandler(Socket socket, int clientNumber) {
+		this.socket = socket;
+		this.clientNumber = clientNumber;
+		System.out.println("New connection with client#" + clientNumber + " at" + socket);
+	}
+	public void run() { // Création de thread qui envoi un message à un client
+		try {
+			DataOutputStream out = new DataOutputStream(socket.getOutputStream()); // création de canal d’envoi
+			DataInputStream input = new DataInputStream(socket.getInputStream());
+			
+			out.writeUTF("Hello from server - you are client#" + clientNumber); // envoi de message
+			
+			// Traitement des commandes
+			String command = input.readUTF();
+			String[] splitCommand = command.split(" ");
+			String result = "";
+
+			// Commande ls
+			if (splitCommand[0].equals("ls")) { 
+				File repertoireCourant = new File(System.getProperty("user.dir"));
+				File[] listFile = repertoireCourant.listFiles();
+				
+				for(File file : listFile) {
+					if (file.isDirectory()) {
+						result += "[Folder] " + file.getName() + "\n";
+					} else if (file.isFile()){
+						result += "[File] " + file.getName() + "\n";
+					}
+					
+				}
+				
+			}
+			
+			// Commande mkdir
+			else if (splitCommand[0].equals("mkdir")) {
+				File repertoireCourant = new File(System.getProperty("user.dir"));
+				String dossierName = splitCommand[1];
+				File Dossier = new File(repertoireCourant, dossierName);
+				boolean res = Dossier.mkdir();
+				
+				if (res) {
+					result = "Le dossier " + dossierName + " a été créé.";
+				} else {
+					result = "Le dossier " + dossierName + " n'a pas été créé.";
+				}
+			}	
+			
+			else {
+				result = "Unknow Command";
+				
+			}
+			
+			out.writeUTF(result);
+
+			
+			
+		} catch (IOException e) {
+			System.out.println("Error handling client# " + clientNumber + ": " + e);
+		} finally {
+			try {
+				socket.close();
+			} catch (IOException e) {
+				System.out.println("Couldn't close a socket, what's going on?");
+			}
+			System.out.println("Connection with client# " + clientNumber + " closed");
+		}
+	}
 }
