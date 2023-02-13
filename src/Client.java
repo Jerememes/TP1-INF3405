e9de28e961d4da26d80652f79e258c4fda2a3adc
Jerome <jerome.laporte@mines-ales.org>
2023-02-13 18:39:19 -0500

    client connexion

--- src/Client.java
+++ src/Client.java
@@ -8,17 +8,18 @@ public class Client {
     private static Socket socket;
     private static DataInputStream in;
     private static DataOutputStream out;
-
+    private static int serverPort;
+    private static String serverAddress;
     public static void main(String[] args) throws Exception {
-        int port = 5000;
-        String serverAddress = "127.0.0.1";
-        socket = new Socket(serverAddress, port);
-
+    	initClient();
+        
+        socket = new Socket(serverAddress, serverPort);
+        
         out = new DataOutputStream(socket.getOutputStream());
         in = new DataInputStream(socket.getInputStream());
         String helloMessageFromServer = in.readUTF();
 
-        System.out.format("Serveur lancé sur [%s:%d] ", serverAddress, port);
+        System.out.format("Serveur lancé sur [%s:%d] ", serverAddress, serverPort);
         System.out.println(helloMessageFromServer);
 
         communication();
@@ -26,7 +27,72 @@ public class Client {
         socket.close();
     }
 
-    public static void communication() {
+    private static void initClient() {
+    	System.out.println("Bonjour !");
+    	Scanner console = new Scanner(System.in);
+        
+
+	        while(true) {
+	            System.out.println("Veuillez rentrer l'adresse IP du serveur : ");
+	            String ip = console.nextLine();
+	            if (isValidIp(ip)) {
+	                serverAddress = ip;
+	                break;
+	            } else {
+	                System.out.println("L'adresse n'est pas valide.");
+	            }
+	        }
+	
+	        while(true) {
+	            System.out.println("Veuillez rentrer le port de connexion : ");
+	            String port = console.nextLine();
+	            if (isValidPort(port)) {
+	                int p = Integer.parseInt(port);
+	                serverPort = p;
+	                break;
+	            } else {
+	            	System.out.println("Le port n'est pas valide.");
+	            }
+	        }
+        
+        	//console.close();
+        
+    }
+
+    private static boolean isValidPort(String port) {
+        try {
+            int number = Integer.parseInt(port);
+            if (number >= 5000 && number <= 5050) {
+                return true;
+            }
+        } catch(NumberFormatException e) {
+            return false;
+        }
+        return false;
+    }
+
+    private static boolean isValidIp(String ip) {
+        boolean result = false;
+        String[] ipNumbers = ip.split("\\.");
+        if (ipNumbers.length == 4) {
+            for(String nb : ipNumbers) {
+                try {
+                    int number = Integer.parseInt(nb);
+                    if (number > 255 || number < 0) {
+                        return false;
+                    }
+                    result = true;
+                } catch(NumberFormatException e) {
+                    return false;
+                }
+            }
+        }
+        return result;
+    }
+		
+	
+
+	public static void communication() {
         Scanner console = new Scanner(System.in);
         while (true) {
             try {
